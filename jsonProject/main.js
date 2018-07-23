97f34a83f3659992c65e832db6327a72d1ecb46a
NeuTrix <neutrix@brandgeneering.com>
2018-07-23 14:11:45 -0700

    tweaks

--- jsonProject/main.js
+++ jsonProject/main.js
@@ -1,20 +1,90 @@
+//   work a csv file
+
+let button = document.createElement("button")
+//  button.innerHTML = "test"
+ let btext = document.createTextNode("battled")
+ button.appendChild(btext)
+ document.body.appendChild(button)
+ let bar = document.getElementById('bar')
+ button.onclick = function() {
+  //  bar.value ='25'
+   let temp = document.createTextNode("25%")
+   bar.removeChild(bar.firstChild)
+   bar.setAttributeNode(document.createAttribute("pickle", 1000))
+   bar.setAttribute('value',5)
+   console.log(bar.attributes)
+   console.log(bar.children)
+
+ }
+ console.log(bar.value)
+ console.log('type: ', bar.parentNode)
+ console.log('children: ', bar.childNodes)
+ console.log('first: ', bar.firstChild.textContent)
+ console.log('last: ', bar.lastChild.textContent)
+ console.log(bar.attributes.length)
+ console.log('get name ==>',bar.getAttribute('max'))
+ console.log(bar.name)
+ for(let i=0; i<bar.attributes.length; i++){
+  console.log(bar.attributes[i])
+ }
+
+
+// === create and append a node =====
+//  let button = document.createElement('button')
+//  let button_text = document.createTextNode("Little");
+//  button.appendChild(button_text);
+//  document.body.appendChild(button)
+
+//  button.onclick = () => {
+//    alert("it worked!!!")
+//  }
+
+//  console.log(window.Cache())
+
 // json object
 
-// ====== Exercise 3.5 ===== basic structure
-let api_key = process.env.API_KEY_OMDB
-let url = `http://www.omdbapi.com/?apikey=${api_key}&t=starwars&y=2016`
-let xhr = new XMLHttpRequest()
-xhr.open('GET', url, 'true');
-xhr.responseType = 'text'; // ensure data format
-xhr.send() // make he request
+// let heading = document.createElement("h1");
+// document.body.appendChild(heading)
+// let doc = document.getElementsByTagName('html')[0]
 
-xhr.onload = () => { // asycnc call back
-  if (xhr.status === 200) { // ensre success
-    let data = JSON.parse(xhr.response)
-    console.log(data)
-    document.getElementById('message').innerHTML = data.city
-  }
-}
+// let list = (node) => {
+//   let kids = node.children
+//   if (kids.length <= 0) {
+//     return 
+//   } else {
+//     for (let i = 0; i < kids.length; i++) {
+//       let long = kids[i].children.length
+//       console.log(long, '==>',kids[i])
+//       list(kids[i])
+//     }
+//   }
+// }
+
+// print(doc)
+// for (let i = 0; i < pars.length; i++) {
+//   console.log(pars[i].innerText)
+// }
+
+// pars.forEach( p => 
+
+  // {return console.log(p)}
+// )
+
+// ====== Exercise 3.5 ===== basic structure
+// let api_key = process.env.API_KEY_OMDB
+// let url = `http://www.omdbapi.com/?apikey=${api_key}&t=starwars&y=2016`
+// let xhr = new XMLHttpRequest()
+// xhr.open('GET', url, 'true');
+// xhr.responseType = 'text'; // ensure data format
+// xhr.send() // make he request
+
+// xhr.onload = () => { // asycnc call back
+//   if (xhr.status === 200) { // ensre success
+//     let data = JSON.parse(xhr.response)
+//     console.log(data)
+//     document.getElementById('message').innerHTML = data.city
+//   }
+// }
 
 
 
