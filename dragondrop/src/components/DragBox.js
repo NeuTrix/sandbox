56545107723bfd9f0ab22d448f22b4fde39c4cd0
NeuTrix <neutrix@brandgeneering.com>
2018-08-10 10:39:52 -0700

    begin buidling to dnd demo

--- dragondrop/src/components/DragBox.js
+++ dragondrop/src/components/DragBox.js
@@ -1,20 +1,84 @@
-import React, { Compnent } from 'react';
+import React, { Component } from 'react';
 import '../App.css'
 
-function DragBox () {
+function handleDrag (event) {
+  event.dataTransfer.setData('text/plain', 'Drag box, yo!')
+  // event.dataTransfer.effectAllowed = 'move';
+  // return alert("Wuzzup!")
+}
+
+function handleDrop (event) {
+  event.preventDefault();
+}
+
+const Drags = (props) => {
   return (
-    <div className= 'grid' >
-      <div className= 'box' id='left'> 
-        <h1> L </h1> 
-      </div>
-      <div className= 'box' id='middle'>
-        <h1> M </h1> 
-      </div>
-      <div className= 'box' id='right'>
-        <h1> R </h1> 
-      </div>
+    <div 
+      className = 'box' 
+      draggable = 'true'
+      onDragStart= {handleDrag}
+    >
+      { props.val } 
     </div>
   )
+}
+
+const Col = (props) => {
+  return (
+    <div 
+      className = "column"
+      dragover = {handleDrop}
+    > 
+      <div className= 'title' >
+        {props.title}
+      </div>
+      <Drags id='left'   val= 'L'/> 
+      <Drags id='middle' val= 'M'/>
+      <Drags id='right'  val= 'R'/>
+    </div>
+  )
+}
+
+class DragBox extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      tasksList: [
+        { category: 'wip', title: 'first' , status: 'pending'},
+        { category: 'wip', title: 'second' , status: 'pending'},
+        { category: 'wip', title: 'third' , status: 'pending'},
+        { category: 'wip', title: 'fourth' , status: 'pending'}
+      ]
+    }
+  }
+
+  render () {
+    let task = {
+      wip: [],
+      comp: []
+    }
+
+    this.state.tasksList.forEach ( item => {
+     task[item.category].push(
+       <div
+        key= {item.title}
+        className= 'draggable'
+        title= {item.title}
+      >
+        {item.title}
+      </div>
+
+     )
+    })
+
+    return (
+    <div className= 'grid' >
+      <Col id='wip' title= "WIP" />
+      <div id='view'/>
+      <Col id= 'done' title= "Done" />
+    </div>
+  )
+  }
 }
 
 export default DragBox
