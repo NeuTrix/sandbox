f36cbcc8a5529b7a264bd15667d0fc2823c0072a
NeuTrix <neutrix@brandgeneering.com>
2018-08-10 12:15:39 -0700

    Fix drag and drop no show items (Chrome)
    The item id needed to be a string and not an int in order to match
    the filtering
    
    Needed to ensure that the event handlers where properly
    bound to the component 'this'

--- dragondrop/src/components/DragBox.js
+++ dragondrop/src/components/DragBox.js
@@ -1,67 +1,72 @@
 import React, { Component } from 'react';
 import '../App.css'
 
-function handleDrag (event) {
-  event.dataTransfer.setData('text/plain', 'Drag box, yo!')
-  // event.dataTransfer.effectAllowed = 'move';
-  // return alert("Wuzzup!")
-}
-
-function handleDrop (event) {
-  event.preventDefault();
-}
-
-const Drags = (props) => {
-  return (
-    <div 
-      className = 'box' 
-      draggable = 'true'
-      onDragStart= {handleDrag}
-    >
-      { props.val } 
-    </div>
-  )
-}
-
-const Col = (props) => {
-  return (
-    <div 
-      className = "column"
-      dragover = {handleDrop}
-    > 
-      <div className= 'title' >
-        {props.title}
-      </div>
-      
-    </div>
-  )
-}
 
 class DragBox extends Component {
   constructor (props) {
     super(props)
     this.state = {
       taskList: [
-        { category: 'wip', title: 'first' , status: 'pending'},
-        { category: 'wip', title: 'second' , status: 'pending'},
-        { category: 'wip', title: 'third' , status: 'pending'},
-        { category: 'wip', title: 'fourth' , status: 'pending'}
+        { id: '0', category: 'wip', title: 'first' , status: 'pending'},
+        { id: '1', category: 'wip', title: 'second' , status: 'pending'},
+        { id: '2', category: 'wip', title: 'third' , status: 'pending'},
+        { id: '3', category: 'wip', title: 'fourth' , status: 'pending'}
       ]
     }
+    this.handleDrag = this.handleDrag.bind(this);
+    this.handleDragOver = this.handleDragOver.bind(this);
+    this.handleDrop = this.handleDrop.bind(this);
   }
 
+  handleDrag(event) {
+    let id = event.target.id
+    event.dataTransfer.setData('id', id)
+    // event.dataTransfer.effectAllowed = 'move';
+    console.log(`Dragging ${id}, B!`)
+    // return alert("Wuzzup!")
+  }
+
+  handleDragOver (event) {
+    event.preventDefault();
+    console.log('daraggging fool!')
+  }
+
+  handleDrop(event) {
+    event.preventDefault();
+    // let id = event.target.id;
+    let id = event.dataTransfer.getData('id')
+    let tasks = this.state.taskList.filter (item => {
+      console.log(`==> need itemId of ${item.id} to match ${id}  `)
+      if (item.id === id) {
+        console.log(`Found it! `)
+        item.category = 'comp';
+      }
+      return item
+    } )
+
+    this.setState({
+      ...this.state, tasks
+    })
+    // console.log(this.state.taskList)
+    console.log(`Dropped item with id:${id} `);
+    console.log(` ${this.state.taskList[0].category}`)
+  }
+  
   render () {
     let tasks = {
       wip: [],
       comp: []
     }
-
+    
     this.state.taskList.forEach ( (item) => {
      tasks[item.category].push(
        <div
-        key= {item.title}
+        key= {item.id}
+        onDragStart= {this.handleDrag}
+        draggable = 'true'
         className= 'box'
         title= {item.title}
+        id= {item.id}
       >
         {item.title}
       </div>
@@ -71,15 +76,26 @@ class DragBox extends Component {
 
     return (
       <div className= 'grid' >
-        <div id='wip' title= "WIP">
-          <span> <h2> WIP </h2> </span>
+
+        <div className= 'column' id='wip' title= "WIP">
+          <span className= 'title'> <h2> WIP </h2> </span>
+          
           {tasks.wip}
         </div>
-        <div id='view'/>
-        <div id='done' title= "Done">
-          <span> <h2> COMP </h2> </span>
+
+        <div id='view'> </div>
+
+        <div 
+          className= 'column' 
+          id='done' 
+          title= "Done" 
+          onDragOver = {this.handleDragOver}
+          onDrop = {this.handleDrop}
+        >
+          <span className= 'title' > <h2> COMP </h2> </span >
           {tasks.comp}
         </div>
+
       </div>
     )
   }
