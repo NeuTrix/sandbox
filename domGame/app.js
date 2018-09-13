da7b3791b5b4e908a9766782a928e80c2082b2ee
NeuTrix <neutrix@brandgeneering.com>
2018-09-12 20:32:56 -0700

    set basic javascript

---
+++ domGame/app.js
@@ -0,0 +1,6 @@
+let numSquares = 6;
+let colors = [];
+let pickedColor;
+let squares = document.querySelectorAll('.square');
+let resetButton = document.querySelector('#reset');
+let mediaButtons = document.querySelectorAll('.mode');
